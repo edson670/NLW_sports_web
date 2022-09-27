75692b683484ceeb86bdb0e3d5c02f2d03fbbf95
edson <j.edson.da.silva.s@gmail.com>
2022-09-27 17:12:11 -0300

    iniciando estilisação da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/main.css'
+import logoImg from './assets/Logo-nlw-sports.svg'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt=""/>
+
+      <h1 className='text-6xl text-white  font-black mt-20 '> Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui</h1>
+
+      <div className="grid grid-cols-6 gap-6">
+    
+          <a href="" className='relative rounded-lg overflow-hidden' >
+            <img src="/game-1.png" alt=""/>
+
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>League of legend</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+            </div>
+          </a>
+
+          <a href="" className='relative rounded-lg overflow-hidden'>
+            <img src="/game-2.png" alt=""/>
+            
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Dota 2</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+            </div>
+          </a>
+          
+          <a href="" className='relative rounded-lg overflow-hidden'>
+            <img src="/game-3.png" alt=""/>
+            
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Couter Strike</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+            </div>
+          </a>
+          
+          <a href="" className='relative rounded-lg overflow-hidden'>
+            <img src="/game-4.png" alt=""/>
+            
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Apex Legends </strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+            </div>
+          </a>
+          
+          <a href="" className='relative rounded-lg overflow-hidden'>
+            <img src="/game-5.png" alt=""/>
+            
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>Fortnite</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+            </div>
+          </a>
+          
+          <a href="" className='relative rounded-lg overflow-hidden'>
+            <img src="/game-6.png" alt=""/>
+            
+            <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+              <strong className='font-bold text-white block'>World of warcraft</strong>
+              <span className='text-zinc-300 text-sm block '>4 anúncios</span>
+            </div>
+          </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
+    
   )
 }
 
